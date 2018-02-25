dab30507a894734581e55642dacd3af39efa89e2
NOTEBOOKGDT\Gerardo Tagliani <nodefined@notebook.com>
2018-02-24 23:48:10 -0300

    ajax functionality for table

--- src/main/java/com/samseng/vehicles/controller/CompanyTypeController.java
+++ src/main/java/com/samseng/vehicles/controller/CompanyTypeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.samseng.vehicles.model.CompanyType;
@@ -31,6 +32,14 @@ public class CompanyTypeController {
 	
 	@Autowired
 	EventObserverJob eventService;
+	
+	@GetMapping("/companytype/table")
+	@ResponseBody
+	//https://datatables.net/examples/ajax/null_data_source.html
+	//https://datatables.net/examples/ajax/custom_data_flat.html
+	public List <CompanyType> dataTable(){
+		return service.findAll();
+	}
 	
 	@GetMapping("/companytype")
     public String findAll(Model model) {
