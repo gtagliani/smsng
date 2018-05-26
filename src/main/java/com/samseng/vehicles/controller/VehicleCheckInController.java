fdaf964999e1055869b9c27d24824a9b237dfc65
colossus\gerardo <noIdentified@nop.com>
2018-05-26 15:27:07 -0300

    no message

--- src/main/java/com/samseng/vehicles/controller/VehicleCheckInController.java
+++ src/main/java/com/samseng/vehicles/controller/VehicleCheckInController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.samseng.vehicles.model.Vehicle;
+import com.samseng.vehicles.model.VehicleRegistered;
 import com.samseng.vehicles.services.DriverService;
 import com.samseng.vehicles.services.VehicleService;
 
@@ -37,6 +40,18 @@ public class VehicleCheckInController {
 	@ResponseBody
 	List<Vehicle> getVehicle(@PathVariable("vehicleId") String vehicleId) {
 		return vehicleService.findByVehicleIdAndNotDeleted(vehicleId);
+	}
+	
+	@RequestMapping(value = "vehiclecheckin/save", method = RequestMethod.POST)
+	@ResponseBody
+	Boolean mainForm(VehicleRegistered vehicleIn) {
+		return true;
+	}
+	
+	@GetMapping("vehiclecheckin/test")
+	@ResponseBody
+	VehicleRegistered test(VehicleRegistered vehicleIn) {
+		return new VehicleRegistered();
 	}
 
 }
